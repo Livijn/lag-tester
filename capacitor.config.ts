5e505dff7a88662aef8477d79459597491403745
Fredrik <ouff@live.se>
2021-12-08 19:54:06 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lag-tester',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
